e04498ddb53fd63a7dca5f24177c282cbcd67ced
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2024-11-09 01:19:42 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const nextAuthSecret = process.env.NEXTAUTH_SECRET;
-const nextAuthUrl = process.env.NEXTAUTH_URL;
+const nextAuthUrl = isProduction ? process.env.NEXTAUTH_URL_PRODUCTION : process.env.NEXTAUTH_URL;
+
 const postgresUrl = process.env.POSTGRES_URL;
 
-const accessToken =process.env.ACCESS_TOKEN;
-const adminEmail =process.env.ADMIN_EMAIL;
-const adminPassword =process.env.ADMIN_PASSWORD;
-
-// const isProduction = process.env.NODE_ENV === 'production'
+const accessToken = process.env.ACCESS_TOKEN;
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminPassword = process.env.ADMIN_PASSWORD;
 
 const nextConfig = {
     env: {
